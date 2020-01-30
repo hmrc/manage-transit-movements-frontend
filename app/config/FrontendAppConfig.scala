efb647934113aaafa9bdddaf7bac166d1e40320c
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2020-01-30 16:47:08 +0000

    CTCTRADERS-251: Add submitArrivalNotificationUrl

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -35,6 +35,9 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val betaFeedbackUrl = s"$contactHost/contact/beta-feedback"
   val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated"
 
+  private val submitArrivalNotificationHost = configuration.get[String]("declare-transit-movement-arrival-frontend.host")
+  val submitArrivalNotificationUrl = s"$submitArrivalNotificationHost/common-transit-convention-trader-arrival/"
+
   lazy val authUrl: String = configuration.get[Service]("auth").baseUrl
   lazy val loginUrl: String = configuration.get[String]("urls.login")
   lazy val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
