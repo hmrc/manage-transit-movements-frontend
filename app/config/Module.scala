822c875564b1e951f6962b99fa3df03767c9add4
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2020-03-03 16:11:21 +0000

    Remove CYA and other utilities.

--- app/config/Module.scala
+++ app/config/Module.scala
@@ -24,12 +24,13 @@ class Module extends AbstractModule {
 
   override def configure(): Unit = {
 
-    bind(classOf[DataRetrievalAction]).to(classOf[DataRetrievalActionImpl]).asEagerSingleton()
-    bind(classOf[DataRequiredAction]).to(classOf[DataRequiredActionImpl]).asEagerSingleton()
-
     // For session based storage instead of cred based, change to SessionIdentifierAction
-    bind(classOf[IdentifierAction]).to(classOf[AuthenticatedIdentifierAction]).asEagerSingleton()
+    bind(classOf[IdentifierAction])
+      .to(classOf[AuthenticatedIdentifierAction])
+      .asEagerSingleton()
 
-    bind(classOf[SessionRepository]).to(classOf[DefaultSessionRepository]).asEagerSingleton()
+    bind(classOf[SessionRepository])
+      .to(classOf[DefaultSessionRepository])
+      .asEagerSingleton()
   }
 }
