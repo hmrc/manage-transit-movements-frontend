f24192d9a1bcc2de43611bc9630755d51a714698
PSouthern <paulsouthern81@hotmail.co.uk>
2020-03-09 09:21:59 +0000

    Separating keys config

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -45,7 +45,7 @@ class FrontendAppConfig @Inject()(configuration: Configuration) {
   lazy val loginContinueUrl: String = configuration.get[String]("urls.loginContinue")
   lazy val destinationUrl: String   = configuration.get[Service]("microservice.services.destination").baseUrl
   lazy val referenceDataUrl: String = configuration.get[Service]("microservice.services.reference-data").baseUrl
-  lazy val enrolmentKey: String     = configuration.get[String]("urls.enrolmentKey")
+  lazy val enrolmentKey: String     = configuration.get[String]("keys.enrolmentKey")
 
   lazy val languageTranslationEnabled: Boolean =
     configuration.get[Boolean]("microservice.services.features.welsh-translation")
