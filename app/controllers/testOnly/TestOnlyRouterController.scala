eae1a4202f16d1322202898fa2c838aabdcc47cc
Matthew Beck <matthew.beck@digital.hmrc.gov.uk>
2020-05-07 09:54:29 +0100

    CTCTRADERS-445: Gov disclaimer

--- app/controllers/testOnly/TestOnlyRouterController.scala
+++ app/controllers/testOnly/TestOnlyRouterController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.testOnly
 
 import connectors.testOnly.TestOnlyRouterConnector
@@ -10,10 +26,10 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.xml.NodeSeq
 
 class TestOnlyRouterController @Inject()(
-                                   cc: ControllerComponents,
-                                   connector: TestOnlyRouterConnector
-                                 )(implicit val ec: ExecutionContext)
-  extends BackendController(cc) {
+  cc: ControllerComponents,
+  connector: TestOnlyRouterConnector
+)(implicit val ec: ExecutionContext)
+    extends BackendController(cc) {
 
   private lazy val logger = Logger(getClass)
 
@@ -30,4 +46,3 @@ class TestOnlyRouterController @Inject()(
       }
   }
 }
-
