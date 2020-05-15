10dbc8f9e548c2788cf4996867673b64e605f213
Chris I'Anson <chris.ianson@digital.hmrc.gov.uk>
2020-05-15 11:07:17 +0100

    Copy over Authorization header

--- app/connectors/testOnly/TestOnlyRouterConnector.scala
+++ app/connectors/testOnly/TestOnlyRouterConnector.scala
@@ -20,7 +20,8 @@ import config.FrontendAppConfig
 import javax.inject.Inject
 import play.api.Logger
 import play.api.mvc.Headers
-import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.http.logging.Authorization
+import uk.gov.hmrc.http.{HeaderCarrier, HttpReads, HttpResponse}
 import uk.gov.hmrc.play.bootstrap.http.HttpClient
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -30,19 +31,27 @@ class TestOnlyRouterConnector @Inject()(val http: HttpClient, config: FrontendAp
 
   val Log = Logger(getClass)
 
-  def submitInboundMessage(requestData: NodeSeq, headers: Headers)(implicit hc: HeaderCarrier): Future[HttpResponse] = {
+  def submitInboundMessage(requestData: NodeSeq, headers: Headers)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
 
     val routerUrl = s"${config.routerUrl}/messages"
-    Log.debug(s"Implicit Headers To Core (Connector): ${hc.headers.toString()}")
-    Log.debug(s"Explicit Headers To Core (Connector): ${headers.headers.toString()}")
-    http.POSTString[HttpResponse](routerUrl, requestData.toString, headers.headers)
+    Log.debug(s"Implicit Headers From Core (Connector): ${headerCarrier.headers.toString()}")
+    Log.debug(s"Explicit Headers From Core (Connector): ${headers.headers.toString()}")
+
+    val newHeaders = headerCarrier
+      .copy(authorization = Some(Authorization(headers.get("Authorization").getOrElse(throw RuntimeException))))
+      .withExtraHeaders(addHeaders(): _*)
+
+    http.POSTString[HttpResponse](routerUrl, requestData.toString)(rds = HttpReads.readRaw, hc = newHeaders, ec = ec)
   }
+
+  private def addHeaders()(
+    implicit headerCarrier: HeaderCarrier): Seq[(String, String)] = Nil
 
   def submitOutboundMessage(requestData: NodeSeq, headers: Headers)(implicit hc: HeaderCarrier): Future[HttpResponse] = {
 
     val serviceUrl = s"${config.destinationUrl}/movements/arrivals"
-    Log.debug(s"Implicit Headers From Core (Connector): ${hc.headers.toString()}")
-    Log.debug(s"Explicit Headers From Core (Connector): ${headers.headers.toString()}")
+    Log.debug(s"Implicit Headers To Core (Connector): ${hc.headers.toString()}")
+    Log.debug(s"Explicit Headers To Core (Connector): ${headers.headers.toString()}")
     http.POSTString[HttpResponse](serviceUrl, requestData.toString, headers.headers)
   }
 }
