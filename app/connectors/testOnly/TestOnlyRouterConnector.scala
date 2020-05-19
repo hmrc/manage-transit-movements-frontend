bc297ac8888c991db9829243fa7b5febabeeba5a
ashwini.rudrappa <ashwini.rudrappa@digital.hmrc.gov.uk>
2020-05-19 11:07:31 +0100

    CTCTRADERS-445: create new end point for unloading remark

--- app/connectors/testOnly/TestOnlyRouterConnector.scala
+++ app/connectors/testOnly/TestOnlyRouterConnector.scala
@@ -37,23 +37,31 @@ class TestOnlyRouterConnector @Inject()(val http: HttpClient, config: FrontendAp
     Log.debug(s"Implicit Headers From Core (Connector): ${headerCarrier.headers.toString()}")
     Log.debug(s"Explicit Headers From Core (Connector): ${headers.headers.toString()}")
 
-//    val newHeaders = headerCarrier
-//      .copy(sessionId = None)
-//      .withExtraHeaders(addHeaders(): _*)
-
     val header = headers.headers.filter(x => x._1 == "X-Message-Sender" || x._1 == "X-Message-Type" || x._1 == "Content-Type")
     Log.debug(s"updated header : $header")
+
     http.POSTString[HttpResponse](routerUrl, requestData.toString, header)
   }
 
   private def addHeaders()(implicit headerCarrier: HeaderCarrier): Seq[(String, String)] = Seq("Content-Type" -> "application/xml")
 
-  def submitOutboundMessage(requestData: NodeSeq, headers: Headers)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
+  def createArrivalNotificationMessage(requestData: NodeSeq, headers: Headers)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
 
-    val serviceUrl = headers.get("arrivalId") match {
-      case Some(arrivalId) if requestData.head.label == "CC044A" => s"${config.destinationUrl}/movements/arrivals/$arrivalId/messages"
-      case _                                                     => s"${config.destinationUrl}/movements/arrivals"
-    }
+    val serviceUrl = s"${config.destinationUrl}/movements/arrivals"
+
+    Log.debug(s"Implicit Headers To Core (Connector): ${headerCarrier.headers.toString()}")
+    Log.debug(s"Explicit Headers To Core (Connector): ${headers.headers.toString()}")
+
+    val newHeaders = headerCarrier
+      .copy(authorization = Some(Authorization(headers.get("Authorization").getOrElse(""))))
+      .withExtraHeaders(addHeaders(): _*)
+
+    http.POSTString[HttpResponse](serviceUrl, requestData.toString)(rds = HttpReads.readRaw, hc = newHeaders, ec = ec)
+  }
+
+  def submitUnloadingRemarkMessage(requestData: NodeSeq, arrivalId: String, headers: Headers)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
+
+    val serviceUrl = s"${config.destinationUrl}/movements/arrivals/$arrivalId/messages"
 
     Log.debug(s"Implicit Headers To Core (Connector): ${headerCarrier.headers.toString()}")
     Log.debug(s"Explicit Headers To Core (Connector): ${headers.headers.toString()}")
