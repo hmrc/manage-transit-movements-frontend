18f203424b779c50f6a879a0492ddc6da4284320
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2020-12-16 08:31:24 +0000

    CTCTRADERS-1585: Added consent as a service

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -29,7 +29,7 @@ class FrontendAppConfig @Inject()(configuration: Configuration) {
   private val contactHost                  = configuration.get[String]("contact-frontend.host")
   private val contactFormServiceIdentifier = "play26frontend"
 
-  val trackingConsentUrl: String = configuration.get[String]("microservice.services.tracking-consent-frontend.url")
+  val trackingConsentUrl: String = configuration.get[Service]("microservice.services.tracking-consent-frontend").baseUrl
   val gtmContainer: String       = configuration.get[String]("microservice.services.tracking-consent-frontend.gtm.container")
 
   val analyticsToken: String         = configuration.get[String](s"google-analytics.token")
