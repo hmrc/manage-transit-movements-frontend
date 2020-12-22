325d2db0a2ca4729b2faac22456e5837093b49cd
PSouthern <paulsouthern81@hotmail.co.uk>
2020-12-22 14:24:44 +0000

    CTCTRADERS-1619 | Adding startUrl back

--- app/config/Service.scala
+++ app/config/Service.scala
@@ -23,7 +23,7 @@ import scala.language.implicitConversions
 final case class Service(host: String, port: String, protocol: String, startUrl: String) {
 
   def baseUrl: String =
-    s"$protocol://$host:$port"
+    s"$protocol://$host:$port/$startUrl"
 
   override def toString: String =
     baseUrl
