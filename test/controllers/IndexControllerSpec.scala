60cd9bb5d7d66abb245c30ff580e764389de5b24
Kevin <29205960+kevincoatsworth@users.noreply.github.com>
2021-09-02 11:26:05 +0100

    CTCTRADERS-2618: Added an index route redirecting to what do you want to do page to cater for 404's

---
+++ test/controllers/IndexControllerSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.{MockNunjucksRendererApp, SpecBase}
+import config.FrontendAppConfig
+import matchers.JsonMatchers
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+
+import scala.concurrent.Future
+
+class IndexControllerSpec extends SpecBase with MockitoSugar with JsonMatchers with MockNunjucksRendererApp {
+
+  val mockFrontendAppConfig = mock[FrontendAppConfig]
+
+  override def beforeEach = {
+    reset(mockFrontendAppConfig)
+    super.beforeEach
+  }
+
+  override def guiceApplicationBuilder(): GuiceApplicationBuilder =
+    super
+      .guiceApplicationBuilder()
+      .overrides(bind(classOf[FrontendAppConfig]).toInstance(mockFrontendAppConfig))
+
+  "return OK and the correct view for a GET" in {
+
+    when(mockNunjucksRenderer.render(any(), any())(any()))
+      .thenReturn(Future.successful(Html("")))
+
+    val request = FakeRequest(GET, routes.IndexController.onPageLoad().url)
+
+    val result = route(app, request).value
+
+    status(result) mustEqual SEE_OTHER
+    redirectLocation(result).value mustEqual routes.WhatDoYouWantToDoController.onPageLoad().url
+  }
+}
