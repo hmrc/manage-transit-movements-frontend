e10929cbdeeffa4c27f619b009b41a16d0fbb184
carrielies <carrielies@users.noreply.github.com>
2021-09-07 09:25:39 +0100

    CTCTRADERS-2616 Add Destinations spec

---
+++ test/models/DeparturesSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import java.time.LocalDateTime
+
+import base.SpecBase
+import models.departure.DepartureStatus.DepartureSubmitted
+import play.api.libs.json.{JsArray, Json}
+import viewModels.DepartureStatus
+
+class DeparturesSpec extends SpecBase {
+
+  "Departures" - {
+
+    "must deserialize" in {
+
+      val dateNow = LocalDateTime.now()
+
+      val json = Json.obj(
+        "retrievedDepartures" -> 1,
+        "totalDepartures"     -> 2,
+        "totalMatched"      -> 3,
+        "departures" -> JsArray(
+          Seq(
+            Json.obj(
+              "arrivalId"               -> 123,
+              "updated"                 -> dateNow,
+              "referenceNumber"         -> "lrn123",
+              "status"                  -> DepartureSubmitted.toString,
+
+            )
+          )
+        )
+      )
+
+      val expectedResult = Departures(
+        1,
+        2,
+        Some(3),
+        Seq(
+          Departure(
+            DepartureId(123),
+            dateNow,
+            LocalReferenceNumber("lrn123"),
+            DepartureSubmitted
+          )
+        )
+      )
+
+      json.validate[Departures].asOpt.value mustEqual expectedResult
+    }
+  }
+}
