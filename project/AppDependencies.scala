1f2ae5ecef955580033416a9a4c629154bd838fa
carrielies <carrielies@users.noreply.github.com>
2021-11-24 12:20:51 +0000

    CTCTRADERS-2801 Update Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,15 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo" %% "play2-reactivemongo"           % "0.20.11-play28",
-    "uk.gov.hmrc"       %% "logback-json-logger"           % "5.1.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.9.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "5.16.0",
-    "uk.gov.hmrc"       %% "play-nunjucks"                 % "0.33.0-play-28",
-    "uk.gov.hmrc"       %% "play-nunjucks-viewmodel"       % "0.15.0-play-28",
-    "org.webjars.npm"            % "govuk-frontend"                   % "3.14.0",
-    "uk.gov.hmrc.webjars"        % "hmrc-frontend"                    % "3.1.1",
-    "com.lucidchart"    %% "xtract"                        % "2.2.1"
+    "org.reactivemongo"    %% "play2-reactivemongo"           % "0.20.13-play28",
+    "uk.gov.hmrc"          %% "logback-json-logger"           % "5.1.0",
+    "uk.gov.hmrc"          %% "play-conditional-form-mapping" % "1.10.0-play-28",
+    "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"    % "5.16.0",
+    "uk.gov.hmrc"          %% "play-nunjucks"                 % "0.33.0-play-28",
+    "uk.gov.hmrc"          %% "play-nunjucks-viewmodel"       % "0.15.0-play-28",
+    "org.webjars.npm"      % "govuk-frontend"                 % "3.14.0",
+    "uk.gov.hmrc.webjars"  % "hmrc-frontend"                  % "3.1.1",
+    "com.lucidchart"       %% "xtract"                        % "2.2.1"
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -22,7 +22,7 @@ object AppDependencies {
     "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "org.pegdown"                 %  "pegdown"                  % "1.6.0",
-    "org.jsoup"                   %  "jsoup"                    % "1.14.2",
+    "org.jsoup"                   %  "jsoup"                    % "1.14.3",
     "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
     "org.mockito"                 %  "mockito-core"             % "3.12.4",
     "org.scalacheck"              %% "scalacheck"               % "1.15.4",
