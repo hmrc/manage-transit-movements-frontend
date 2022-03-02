99085b004009f356592a61f6fd433df3560d4e39
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-03-02 14:48:40 +0000

    CTCTRADERS-2908: Appending service name to beta feedback url.

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -33,8 +33,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val userResearchUrl: String         = configuration.get[String]("urls.userResearch")
   val showUserResearchBanner: Boolean = configuration.get[Boolean]("banners.showUserResearch")
 
-  val betaFeedbackUrl                = s"$contactHost/contact/beta-feedback"
-  val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated"
+  val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated?service=$contactFormServiceIdentifier"
 
   val signOutUrl: String = configuration.get[String]("urls.logoutContinue") + configuration.get[String]("urls.feedback")
 
