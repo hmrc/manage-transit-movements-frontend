095a667b53e7bc9bf7dba6921b0508f7f661e1dd
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-05-10 07:53:27 +0100

    Using migrated view in no release for transit controller.

--- test/controllers/departure/NoReleaseForTransitControllerSpec.scala
+++ test/controllers/departure/NoReleaseForTransitControllerSpec.scala
@@ -20,17 +20,15 @@ import base.SpecBase
 import generators.Generators
 import matchers.JsonMatchers
 import models.departure.NoReleaseForTransitMessage
-import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.{reset, times, verify, when}
+import org.mockito.Mockito.{reset, when}
 import org.scalacheck.Arbitrary.arbitrary
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.json.{JsObject, Json}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.twirl.api.Html
 import services.DepartureMessageService
+import views.html.NoReleaseForTransitView
 
 import scala.concurrent.Future
 
@@ -56,26 +54,19 @@ class NoReleaseForTransitControllerSpec extends SpecBase with JsonMatchers with 
 
       val transitMessage = arbitrary[NoReleaseForTransitMessage].sample.value
 
-      when(mockNunjucksRenderer.render(any(), any())(any()))
-        .thenReturn(Future.successful(Html("")))
-
       when(mockDepartureMessageService.noReleaseForTransitMessage(any())(any()))
         .thenReturn(Future.successful(Some(transitMessage)))
 
-      val request                                = FakeRequest(GET, routes.NoReleaseForTransitController.onPageLoad(departureId).url)
-      val templateCaptor: ArgumentCaptor[String] = ArgumentCaptor.forClass(classOf[String])
-      val jsonCaptor: ArgumentCaptor[JsObject]   = ArgumentCaptor.forClass(classOf[JsObject])
+      val request = FakeRequest(GET, routes.NoReleaseForTransitController.onPageLoad(departureId).url)
 
       val result = route(app, request).value
 
+      val view = injector.instanceOf[NoReleaseForTransitView]
+
       status(result) mustEqual OK
 
-      verify(mockNunjucksRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
-
-      val expectedJson = Json.obj("noReleaseForTransitMessage" -> Json.toJson(transitMessage))
-
-      templateCaptor.getValue mustEqual "noReleaseForTransit.njk"
-      jsonCaptor.getValue must containJson(expectedJson)
+      contentAsString(result) mustEqual
+        view(transitMessage)(request, messages).toString
     }
 
     "render Technical difficulties page on failing to fetch noReleaseForTransitMessage" in {
