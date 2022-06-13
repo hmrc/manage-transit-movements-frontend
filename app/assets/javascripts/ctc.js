caa655b8323dae76f117e5fc9b75ebae4caba0f6
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-06-13 09:22:06 +0100

    CTCP-629: Simplify back link and remove redundant code

--- app/assets/javascripts/ctc.js
+++ app/assets/javascripts/ctc.js
@@ -1,15 +1,13 @@
-document.body.className = ((document.body.className) ? document.body.className + ' js-enabled' : 'js-enabled');
-
 // Find first ancestor of el with tagName
 // or undefined if not found
 function upTo(el, tagName) {
     tagName = tagName.toLowerCase();
 
     while (el && el.parentNode) {
-      el = el.parentNode;
-      if (el.tagName && el.tagName.toLowerCase() == tagName) {
-        return el;
-      }
+        el = el.parentNode;
+        if (el.tagName && el.tagName.toLowerCase() == tagName) {
+            return el;
+        }
     }
 
     // Many DOM methods return null if they don't
@@ -17,10 +15,8 @@ function upTo(el, tagName) {
     // It would be OK to omit the following and just
     // return undefined
     return null;
-  }
+}
 
-// initialise GovUK lib
-GOVUKFrontend.initAll();
 
 // back link
 var backLink = document.querySelector('.govuk-back-link');
