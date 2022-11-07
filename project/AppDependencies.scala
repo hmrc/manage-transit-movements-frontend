f8f7f41a601064c01a73f5bed5cb64cc2ea4a8e5
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-11-07 13:08:06 +0000

    Implementing platops suggestions.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,6 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"          %% "logback-json-logger"           % "5.2.0",
     "uk.gov.hmrc"          %% "play-conditional-form-mapping" % "1.12.0-play-28",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"    % bootstrapVersion,
     "uk.gov.hmrc"          %% "play-frontend-hmrc"            % "3.32.0-play-28",
