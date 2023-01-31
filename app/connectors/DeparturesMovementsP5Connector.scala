dfce31e1cf53d6e971ca1d89fcb6dc49fc627160
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-01-31 10:13:37 +0000

    CTCP-1526: P5 connector availability wiremocks

--- app/connectors/DeparturesMovementsP5Connector.scala
+++ app/connectors/DeparturesMovementsP5Connector.scala
@@ -20,26 +20,25 @@ import config.FrontendAppConfig
 import logging.Logging
 import models.{DeparturesSummary, DraftAvailability}
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpReads}
+import uk.gov.hmrc.http.HttpReads.is2xx
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class DeparturesMovementsP5Connector @Inject() (config: FrontendAppConfig, http: HttpClient)(implicit ec: ExecutionContext) extends Logging {
 
-  def getDeparturesSummary(queryParams: Seq[(String, String)])(implicit hc: HeaderCarrier): Future[Option[DeparturesSummary]] = {
+  def getDeparturesSummary(queryParams: Seq[(String, String)] = Seq.empty)(implicit hc: HeaderCarrier): Future[Option[DeparturesSummary]] = {
     val url = s"${config.draftDeparturesUrl}/user-answers"
 
-    http
-      .GET[DeparturesSummary](url, queryParams)(HttpReads[DeparturesSummary], hc, ec)
-      .map(
-        departureSummaries => Some(departureSummaries)
-      )
-      .recover {
-        case _ =>
-          logger.error(s"get Departure Summary failed to return data")
-          None
-      }
+    http.GET[HttpResponse](url, queryParams).map {
+      case response if is2xx(response.status) =>
+        response.json.asOpt[DeparturesSummary]
+      case response if response.status == 404 =>
+        Some(DeparturesSummary(List.empty))
+      case _ =>
+        None
+    }
   }
 
   def getDraftDeparturesAvailability()(implicit hc: HeaderCarrier): Future[DraftAvailability] =
