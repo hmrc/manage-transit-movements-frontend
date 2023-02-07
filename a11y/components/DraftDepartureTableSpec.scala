206208bd6eb9b82942a40ee4cf34892ffefa93ad
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-02-07 15:51:30 +0000

    CTCP-1884: a11y tests

--- a11y/components/DraftDepartureTableSpec.scala
+++ a11y/components/DraftDepartureTableSpec.scala
@@ -29,16 +29,13 @@ class DraftDepartureTableSpec extends A11ySpecBase {
     val component = app.injector.instanceOf[DraftDeparturesTable]
 
     val title                     = nonEmptyString.sample.value
-    val visuallyHiddenHeader      = nonEmptyString.sample.value
-    val rowHeadingReferenceNumber = nonEmptyString.sample.value
-    val rowHeadingDaysToComplete  = nonEmptyString.sample.value
 
     "pass accessibility checks" when {
 
       "draft departure table" in {
-        val dataRows = arbitrary[AllDraftDeparturesViewModel].sample.value.dataRows
+        val dataRows = arbitrary[AllDraftDeparturesViewModel].sample.value
         val content = template.apply(title) {
-          component.apply(dataRows, visuallyHiddenHeader, rowHeadingReferenceNumber, rowHeadingDaysToComplete).withHeading(title)
+          component.apply(dataRows).withHeading(title)
         }
         content.toString() must passAccessibilityChecks
       }
