0f00b71f5cad10682f3c25a7c69a73b7d7a3f8de
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-02-20 11:22:37 +0000

    CTCP-2267: wip adding testOnly endpoints for API connection

---
+++ app/connectors/testOnly/TestOnlyP5ArrivalsAPIConnector.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.testOnly
+
+import config.FrontendAppConfig
+import play.api.mvc.Headers
+import uk.gov.hmrc.http.{Authorization, HeaderCarrier, HttpClient, HttpReads, HttpResponse}
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.xml.NodeSeq
+
+class TestOnlyP5ArrivalsAPIConnector @Inject() (val http: HttpClient, config: FrontendAppConfig)(implicit ec: ExecutionContext) {
+
+  def arrivalOutbound(requestData: NodeSeq)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
+    val serviceUrl = s"${config.commonTransitConventionTradersUrl}movements/arrivals"
+
+    http.POSTString[HttpResponse](serviceUrl, requestData.toString)
+  }
+
+  def arrivalInbound(requestData: NodeSeq, arrivalId: String, headers: Headers)(implicit headerCarrier: HeaderCarrier): Future[HttpResponse] = {
+
+    val serviceUrl = s"${config.commonTransitConventionTradersUrl}transit-movements/traders/movements/$arrivalId/messages"
+
+    http.POSTString[HttpResponse](serviceUrl, requestData.toString)(
+      rds = HttpReads[HttpResponse],
+      hc = newHeaders(headers),
+      ec = ec
+    )
+  }
+}
---
+++ app/controllers/testOnly/TestOnlyP5ArrivalsAPIController.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.testOnly
+
+import connectors.testOnly.TestOnlyArrivalsRouterConnector
+import play.api.mvc.{Action, DefaultActionBuilder, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+import scala.xml.NodeSeq
+
+class TestOnlyP5ArrivalsAPIController @Inject() (
+  cc: MessagesControllerComponents,
+  connector: TestOnlyArrivalsRouterConnector,
+  action: DefaultActionBuilder
+)(implicit val ec: ExecutionContext)
+    extends FrontendController(cc) {
+
+  def arrivalNotificationMessageToCore: Action[NodeSeq] = action.async(parse.xml) {
+    implicit request =>
+      connector
+        .createArrivalNotificationMessage(request.body, request.headers)
+        .map {
+          response =>
+            val location = response.header("Location").getOrElse("Location is missing")
+            Status(response.status)
+              .withHeaders(
+                "Location"  -> location,
+                "arrivalId" -> location.split("/").last
+              )
+        }
+  }
+
+  def resubmitArrivalNotificationMessageToCore: Action[NodeSeq] = action.async(parse.xml) {
+    implicit request =>
+      request.headers.get("arrivalId") match {
+        case Some(arrivalId) =>
+          connector
+            .resubmitArrivalNotificationMessage(request.body, arrivalId, request.headers)
+            .map {
+              response =>
+                val location = response.header("Location").getOrElse("Location is missing")
+                Status(response.status)
+                  .withHeaders(
+                    "Location"  -> location,
+                    "arrivalId" -> location.split("/").last
+                  )
+            }
+
+        case _ => Future.successful(BadRequest("ArrivalId is missing"))
+      }
+  }
+
+  def messageToCore: Action[NodeSeq] = action.async(parse.xml) {
+    implicit request =>
+      request.headers.get("arrivalId") match {
+        case Some(arrivalId) =>
+          connector
+            .submitMessageToCore(request.body, arrivalId, request.headers)
+            .map(
+              response => Status(response.status)
+            )
+
+        case _ => Future.successful(BadRequest("ArrivalId is missing"))
+      }
+  }
+}
