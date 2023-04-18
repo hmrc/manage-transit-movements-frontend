e4dc842ae6745b3f6b9e49404139ee027dd4b1fb
srinivasulumunagalahmrc <srinivasulu.munagala@digital.hmrc.gov.uk>
2023-04-18 09:31:20 +0100

    CTCP-2831 controller

---
+++ app/viewModels/P5/departure/GoodsUnderControlP5ViewModel.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewModels.P5.departure
+
+import models.departureP5.IE060Data
+import play.api.i18n.Messages
+
+import javax.inject.Inject
+
+case class GoodsUnderControlP5ViewModel(ie060Data: Option[IE060Data])
+
+object GoodsUnderControlP5ViewModel {
+
+  def apply(ie60Data: IE060Data)(implicit messages: Messages): GoodsUnderControlP5ViewModel =
+    new GoodsUnderControlP5ViewModelProvider()(ie60Data)
+
+  class GoodsUnderControlP5ViewModelProvider @Inject() () {
+
+    def apply(ie60Data: IE060Data)(implicit messages: Messages): GoodsUnderControlP5ViewModel = {
+      val helper = new GoodsUnderControlP5Helper(ie60Data)
+
+
+
+      new GoodsUnderControlP5ViewModel(Seq(headerSection, commentsSection))
+    }
+  }
+}
