1270d2cba69c2fdf2fbe23828b79c9016427a30c
ajchilts <40767309+ajchilts@users.noreply.github.com>
2023-04-20 16:42:11 +0100

    UR banner override

---
+++ app/viewHelpers/UserResearchBannerHelper.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewHelpers
+
+import config.FrontendAppConfig
+import play.api.i18n.Messages
+import play.twirl.api.Html
+import views.html.components
+
+import javax.inject.Inject
+
+class UserResearchBannerHelper @Inject() (
+  customUserResearchBanner: components.CustomUserResearchBanner
+) {
+
+  def ctcUrBanner()(implicit messages: Messages, appConfig: FrontendAppConfig): Option[Html] =
+    if (appConfig.showUserResearchBanner) {
+      Some(
+        customUserResearchBanner(
+          bannerTitle = messages("manage.user.banner.helpImprove"),
+          signUpMessage = messages("manage.user.banner.emcs.signUp"),
+          signUpUrl = appConfig.userResearchUrl,
+          noThanksMessage = messages("manage.user.banner.noThanks")
+        )
+      )
+    } else {
+      None
+    }
+}
