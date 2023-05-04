9dd6cdf3a141775eca5d20e2e39d2bbb8bbc4fb0
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-05-04 15:31:32 +0100

    CTCP-2795: Created initial page with content and dynamic errors

---
+++ app/viewModels/P5/departure/ReviewDepartureErrorMessageP5ViewModel.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewModels.P5.departure
+
+import models.departureP5.IE056MessageData
+import play.api.i18n.Messages
+import services.ReferenceDataService
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.RejectionMessageP5MessageHelper
+import viewModels.sections.Section
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+case class ReviewDepartureErrorMessageP5ViewModel(sections: Seq[Section], lrn: String, multipleErrors: Boolean) {
+
+  def title(implicit messages: Messages): String = messages("departure.ie056.review.message.title")
+
+  def heading(implicit messages: Messages): String = messages("departure.ie056.review.message.heading")
+
+  def paragraph1Prefix(implicit messages: Messages): String = messages("departure.ie056.review.message.paragraph1.prefix", lrn)
+
+  def paragraph1Suffix(implicit messages: Messages): String = if (multipleErrors) {
+    messages(
+      "departure.ie056.review.message.paragraph1.plural.suffix"
+    )
+  } else {
+    messages(
+      "departure.ie056.review.message.paragraph1.singular.suffix"
+    )
+  }
+
+  def paragraph2Prefix(implicit messages: Messages): String = messages("departure.ie056.review.message.paragraph2.prefix")
+  def paragraph2Link(implicit messages: Messages): String   = messages("departure.ie056.review.message.paragraph2.link")
+
+  def paragraph2Suffix(implicit messages: Messages): String = if (multipleErrors) {
+    messages("departure.ie056.review.message.paragraph2.singular.suffix")
+  } else {
+    messages("departure.ie056.review.message.paragraph2.plural.suffix")
+  }
+
+  def hyperlink(implicit messages: Messages): String = messages("departure.ie056.review.message.hyperlink")
+
+}
+
+object ReviewDepartureErrorMessageP5ViewModel {
+
+  class ReviewDepartureErrorMessageP5ViewModelProvider @Inject() (referenceDataService: ReferenceDataService) {
+
+    def apply(
+      ie056MessageData: IE056MessageData,
+      lrn: String
+    )(implicit messages: Messages, ec: ExecutionContext, hc: HeaderCarrier): Future[ReviewDepartureErrorMessageP5ViewModel] = {
+      val helper = new RejectionMessageP5MessageHelper(ie056MessageData, referenceDataService)
+
+      val multipleErrors = ie056MessageData.functionalErrors.length > 1
+      val sections       = Seq(helper.errorSection())
+      Future
+        .sequence(sections)
+        .map(
+          sec => ReviewDepartureErrorMessageP5ViewModel(sec, lrn, multipleErrors)
+        )
+
+    }
+  }
+}
