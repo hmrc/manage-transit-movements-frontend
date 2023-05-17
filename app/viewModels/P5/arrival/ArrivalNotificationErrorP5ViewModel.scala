5dc3f397f68ca169adc4073b0a42c7c3d5c9146c
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2023-05-17 14:44:57 +0100

    CTCP-2918: create page for Arrival Notification Display no errors

---
+++ app/viewModels/P5/arrival/ArrivalNotificationErrorP5ViewModel.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewModels.P5.arrival
+
+import play.api.i18n.Messages
+
+case class ArrivalNotificationErrorP5ViewModel(mrn: String, noErrors: Boolean) {
+  def title(implicit messages: Messages): String = messages("arrival.notification.errors.message.title")
+
+  def heading(implicit messages: Messages): String = messages("arrival.notification.errors.message.heading")
+
+  def paragraph1(implicit messages: Messages): String =
+    if (noErrors) {
+      messages("arrival.notification.errors.message.noerrors", mrn)
+    } else {
+      messages("arrival.notification.errors.message.elevenpluserrors", mrn)
+    }
+
+  def paragraph2(implicit messages: Messages): String = messages("arrival.notification.errors.message.paragraph2")
+
+  def paragraph3Prefix(implicit messages: Messages): String = messages("arrival.notification.errors.message.paragraph3.prefix")
+  def paragraph3Suffix(implicit messages: Messages): String = messages("arrival.notification.errors.message.paragraph3.suffix")
+  def paragraph3Link(implicit messages: Messages): String   = messages("arrival.notification.errors.message.paragraph3.link")
+
+  def hyperlink(implicit messages: Messages): String = messages("arrival.notification.errors.message.hyperlink")
+
+}
+
+object ArrivalNotificationErrorP5ViewModel {
+
+  class ArrivalNotificationErrorP5ViewModelProvider {
+    def apply(mrn: String, noErrors: Boolean): ArrivalNotificationErrorP5ViewModel = ArrivalNotificationErrorP5ViewModel(mrn, noErrors)
+  }
+
+}
