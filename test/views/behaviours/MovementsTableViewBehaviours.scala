a8239dcafe344f8125e0cfd0c40b098083dedea9
srinivasulumunagalahmrc <srinivasulu.munagala@digital.hmrc.gov.uk>
2023-07-11 12:22:19 +0100

    CTCP-3300 removed apply in Submission

--- test/views/behaviours/MovementsTableViewBehaviours.scala
+++ test/views/behaviours/MovementsTableViewBehaviours.scala
@@ -90,6 +90,13 @@ trait MovementsTableViewBehaviours[T <: ViewMovement] extends ViewBehaviours wit
                 behave like elementWithHiddenText(status, messages(s"$prefix.table.status"))
               }
 
+              "must display correct status when args present" in {
+                val status = row.selectFirst("td[data-testrole*=-status]")
+
+                behave like elementWithVisibleText(status, viewMovement.status)
+                behave like elementWithHiddenText(status, messages(s"$prefix.table.status", s"$prefix.table.args"))
+              }
+
               "must display actions" - {
                 val actions = row.selectFirst("td[data-testrole*=-actions]")
 
