d54c214d3d37f355242ef7521125d17cbea8408e
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2024-02-16 14:20:14 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30"   % "2.0.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "8.0.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "8.5.0",
     "com.lucidchart"       %% "xtract"                                  % "2.3.0"
   )
 
