dcf6a283aa89c81000af6f3a5639dfd32c5fb3ff
prashanth <Prashanth.Mamidi@gmail.com>
2024-07-09 12:52:00 +0100

    [CTCP-5552] added incident details page

--- test/base/SpecBase.scala
+++ test/base/SpecBase.scala
@@ -44,7 +44,6 @@ trait SpecBase extends AnyFreeSpec with Matchers with OptionValues with TryValue
   val departureIdP5: String    = "643cffea2dca70b2"
   val arrivalIdP5: String      = "62f4ebbb765ba8c2"
 
-//  val incidentIndex: Int = 0  // TODO
   val incidentIndex: Index = Index(0)
 
   val fakeCustomsOffice: CustomsOffice = CustomsOffice("1234", "Customs Office", Some("01234567"))
