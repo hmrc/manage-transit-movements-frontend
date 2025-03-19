e16c5ce56c8f672228695b9945901b0486de5952
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-03-19 14:26:12 +0000

    CTCP-3254 Add customs and  space in between the officeName and officeId and
    Signed-off-by: Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>

--- test/viewModels/P5/departure/GoodsUnderControlP5ViewModelSpec.scala
+++ test/viewModels/P5/departure/GoodsUnderControlP5ViewModelSpec.scala
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -225,7 +241,7 @@ class GoodsUnderControlP5ViewModelSpec extends SpecBase with AppWithDefaultMockF
 
           val result = viewModel(customsOffice).customsOfficeContent
 
-          result `mustBe` s"You must share the requested documentation with the office of departure. Contact Customs at Dover(CD123) on 00443243543 or test123@gmail.com."
+          result `mustBe` s"You must share the requested documentation with the customs office of departure. Contact Customs at Dover (CD123) on 00443243543 or test123@gmail.com."
         }
       }
 
@@ -241,7 +257,7 @@ class GoodsUnderControlP5ViewModelSpec extends SpecBase with AppWithDefaultMockF
 
           val result = viewModel(customsOffice).customsOfficeContent
 
-          result `mustBe` s"You must share the requested documentation with the office of departure. Contact Customs at Dover(CD123) on test123@gmail.com."
+          result `mustBe` s"You must share the requested documentation with the customs office of departure. Contact Customs at Dover (CD123) on test123@gmail.com."
         }
       }
 
@@ -257,7 +273,7 @@ class GoodsUnderControlP5ViewModelSpec extends SpecBase with AppWithDefaultMockF
 
           val result = viewModel(customsOffice).customsOfficeContent
 
-          result `mustBe` s"You must share the requested documentation with the office of departure. Contact Customs at Dover(CD123) on 00443243543."
+          result `mustBe` s"You must share the requested documentation with the customs office of departure. Contact Customs at Dover (CD123) on 00443243543."
         }
       }
 
@@ -272,7 +288,7 @@ class GoodsUnderControlP5ViewModelSpec extends SpecBase with AppWithDefaultMockF
 
           val result = viewModel(customsOffice).customsOfficeContent
 
-          result `mustBe` s"You must share the requested documentation with the office of departure. Contact Customs at Dover(CD123)."
+          result `mustBe` s"You must share the requested documentation with the customs office of departure. Contact Customs at Dover (CD123)."
         }
       }
     }
