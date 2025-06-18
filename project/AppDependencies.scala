274af6b4dd4c22356c3983698fb9b23ffe83d758
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2025-06-18 11:58:12 +0100

    CTCP-6554: Change Accept header version to 1.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30"   % "3.3.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"              % bootstrapVersion,
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "12.4.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"              % "12.6.0",
     "org.webjars.npm"       % "ministryofjustice__frontend"             % "5.1.3",
     "javax.xml.bind"        % "jaxb-api"                                % "2.3.1",
     "org.typelevel"        %% "cats-core"                               % "2.13.0",
@@ -20,11 +20,11 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"              %% "scalatest"                % "3.2.19",
     "uk.gov.hmrc"                %% "bootstrap-test-play-30"   % bootstrapVersion,
-    "org.mockito"                 % "mockito-core"             % "5.16.1",
+    "org.mockito"                 % "mockito-core"             % "5.18.0",
     "org.scalatestplus"          %% "mockito-5-12"             % "3.2.19.0",
     "org.scalacheck"             %% "scalacheck"               % "1.18.1",
     "org.scalatestplus"          %% "scalacheck-1-18"          % "3.2.19.0",
-    "org.jsoup"                   % "jsoup"                    % "1.19.1",
+    "org.jsoup"                   % "jsoup"                    % "1.20.1",
     "io.github.wolfendale"       %% "scalacheck-gen-regexp"    % "1.1.0",
     "org.apache.pekko"           %% "pekko-testkit"            % pekkoVersion,
     "org.apache.pekko"           %% "pekko-stream-testkit"     % pekkoVersion
